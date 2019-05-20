389e46bac11d71dcc20c80b99e4e35fbbcc84f10
Zhiqiang Fu <fuzhiqiang1230@outlook.com>
2019-05-20 11:57:56 +0800

    end?
    似乎好了

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import os
-import subprocess
 from ctypes import *
-import sys
-
-
-
-
-
 if __name__ == "__main__":
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    print(current_dir)
-    sys.path.append(current_dir)
-    sys.path.append(current_dir+'\libusb-1.0')
     dll = cdll.LoadLibrary('G:\\mycode\\usb\\usbdll.dll')
     z = dll.main()
 
